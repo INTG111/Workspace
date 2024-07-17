217b522a1838d88196d8affb80b138c0deeb1a64
陳奕璋 <t095678@gmail.com>
2024-07-17 20:39:48 +0800

    回應特定字詞

---
+++ DiscordBot.py
@@ -0,0 +1,24 @@
+#導入 Discord.py
+import discord
+#client 是我們與 Discord 連結的橋樑，intents 是我們要求的權限
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+#調用 event 函式庫
+@client.event
+#當機器人完成啟動時
+async def on_ready():
+    print('目前登入身份：', client.user)
+
+@client.event
+#當有訊息時
+async def on_message(message):
+    #排除自己的訊息，避免陷入無限循環
+    if message.author == client.user:
+        return
+    #如果包含 ping，機器人回傳 pong
+    if message.content == 'Hello':
+        await message.channel.send('Hello World')
+
+client.run('MTIxODAxODMxMzkwMTMxNDEyOA.GD7BlG.wobWaoRA4rjup4PHV1pGEOlmrfb7C7IOSm3dWM') #TOKEN 在剛剛 Discord Developer 那邊「BOT」頁面裡面
